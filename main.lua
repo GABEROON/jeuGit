e26911da3107645e4220ddf2f1cddeba0eb6f481
GABEROON <gabrielcloutier97@gmail.com>
2017-04-07 16:11:23 -0400

    Ajout d'obstacles et tentatives d'en avoir plusieurs

--- main.lua
+++ main.lua
@@ -19,13 +19,18 @@
 --local monPerso = machineOgre:new()
 local circleEtatTouch = display.newGroup() -- créer un nouveau display pour le cercle de collision
 local perso = display.newGroup() -- créer un nouveau display groupe pour le perso
-function circleEtatTouch:init()
-    -- etat
-     
+local obstacle = display.newGroup() -- créer un nouveau display groupe obstacle
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------- ETAT TOUCH -----------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+function circleEtatTouch:init() 
     local circle = display.newCircle(display.contentWidth/2,display.contentHeight/2,200)
     circle: setFillColor(1,1,0.8)
     self:insert(circle)
 end
+
 function circleEtatTouch:touch(e)
     if e.phase == 'began' then 
         perso:changerEtat()        
@@ -33,7 +38,50 @@ function circleEtatTouch:touch(e)
 end -- touch
 
 
--- display.newLine( x1, y1, x2, y2 )
+------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------- OBSTACLE -----------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+function obstacle:init()
+    print('init')
+    
+    timer.performWithDelay(1000,self:spawn(),-1)
+    
+end
+function obstacle:spawn()
+    print('spawn')
+    
+    local posX = {display.contentWidth/2-display.contentWidth/3,display.contentWidth/2,display.contentWidth/2+display.contentWidth/3} -- {128, 384, 640}
+    local posY = math.random(150) -- 1 à 150
+    local colActuelle = math.random(3) -- 1 à 3
+    local width = 200
+    local height = 50
+    self.rect = display.newRect( posX[colActuelle], posY, width, height )
+    self.rect:setFillColor(1,1,1)
+    self:insert(self.rect)
+    self.vitesse = 15
+    self.alive = true
+end
+function obstacle:enterFrame()
+    if self.alive then --si l'obstacle existe
+        obstacle:render()
+    end
+end
+function obstacle:render()
+    self.y = self.y + self.vitesse
+    if self.y > display.contentHeight + self.height and self.alive then -- si est en dehors de la hauteur de l'écran + sa propre hauteur, kill()
+        obstacle:kill()
+    end
+end
+function obstacle:kill()
+    self:removeSelf()
+    self.alive = false
+    print('kill')
+end
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------ PERSO -----------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+
 function perso:init() -- déclaration de la fonction init
     -- graphique 
     self.circle = display.newCircle(0,display.contentHeight/2+display.contentWidth/2,50)
@@ -42,14 +90,14 @@ function perso:init() -- déclaration de la fonction init
     -- mouvement
     self.currentPos = 2
     self.tPositions = {display.contentWidth/2-display.contentWidth/3,display.contentWidth/2,display.contentWidth/2+display.contentWidth/3} -- {128, 384, 640}
+    self.vitesse = 40
 
     -- etat
     self.tEtat = {1,2,3}
     self.currentEtat = 1
-    --self:insert(self.circleEtatTouch)
+    
 
-    --print(self.currentPos)
-    --print(self.tPositions)
+   
 end
 
 function perso:changerEtat()
@@ -74,13 +122,12 @@ end
 
 function perso:render() -- est appelé à chaque enterFrame
     self.x = self.tPositions[self.currentPos] -- place son x par rapport a currentPos à l'intérieur de tPositions
-    -- --print(self.x)
+   
 end
 function perso:touch(e) -- appele la fonction render à chaque fois qu'un nouveau frame est créé
-    -----print(e)
     if e.phase == 'began' then
         if(e.x>display.contentWidth/2) then
-            --print('droite')
+           
             if self.currentPos ~= 3 then
                 self.currentPos = self.currentPos + 1
             end
@@ -95,7 +142,9 @@ function perso:touch(e) -- appele la fonction render à chaque fois qu'un nouvea
     end -- if
 end -- function
 
-
+------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------ RUNTIME ET INIT -------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 -- interface:init()
@@ -103,9 +152,12 @@ end -- function
 
 
 perso:init()
+obstacle:init()
+
 circleEtatTouch:init()
 
 Runtime:addEventListener('enterFrame', perso) 
+Runtime:addEventListener('enterFrame', obstacle) 
 Runtime:addEventListener('touch', perso) 
 circleEtatTouch:addEventListener('touch', circleEtatTouch) 
 
